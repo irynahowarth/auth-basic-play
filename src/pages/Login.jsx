1413131f9c7024d2ea0c221ab39a2c98dfa58920
Iryna Howarth <irynahowarth@gmail.com>
2024-10-03 19:16:57 +0100

    [style]: add styles for Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,31 +3,56 @@ import { Form, useNavigation, Link } from 'react-router-dom'
 
 export default function Login() {
     const navigate = useNavigation()
+    const inputStyles = "block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 placeholder:text-sm focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
+    const labelStyles = "mb-3 block text-sm font-medium text-gray-700"
+
   return (
-    <div>
-        <h2>Log in to your accout</h2>
-        <Form method="post"/>
-        <label htmlFor="email">Email</label>
-            <input 
-                name="email"
-                id="email"
-                type="email" 
-                placeholder='Enter your email address'
-            />
-            <label htmlFor="password">Password</label>
-            <input 
-                name="password"
-                id="password"
-                type="password" 
-                placeholder='Enter your password'
-            />
-            <button disabled={navigate.state=== "submitting"}>
-              {navigate.state === "submitting"
-                ? "Logging in..." 
-                : "Log in"
-              }
-            </button>
-            <Link to="/signup">Create a new account</Link>
+    <div className="relative flex min-h-full shrink-0 justify-center md:px-12 lg:px-0">
+      <div className="relative z-10 flex flex-1 flex-col bg-white px-4 py-10 shadow-2xl sm:justify-center md:flex-none md:px-28">
+        <div className='mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0'>
+            <h2 className="text-lg font-semibold text-gray-900">Sign in to your account</h2>
+            <p className="mt-2 text-sm text-gray-700">
+              Don’t have an account?{" "} 
+              <Link aria-label="Signup" to="/signup" className="font-medium text-blue-600 hover:underline">Sign up</Link>{" "}
+              for free.</p>
+            <Form method="post" className="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2">
+              <div className="col-span-full">
+                <label htmlFor="email" className={labelStyles}>Email</label>
+                <input 
+                    name="email"
+                    id="email"
+                    type="email" 
+                    placeholder='Enter your email address'
+                    className={inputStyles}
+                    required
+                    autoComplete='email'
+                />
+              </div>
+              <div className="col-span-full">
+                <label htmlFor="password" className={labelStyles}>Password</label>
+                <input 
+                    name="password"
+                    id="password"
+                    type="password" 
+                    placeholder='Enter your password'
+                    className={inputStyles}
+                    required
+                />
+              </div>
+              <div className="col-span-full">
+                <button 
+                  disabled={navigate.state=== "submitting"}
+                  className="inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600 w-full"
+                >
+                  {navigate.state === "submitting"
+                    ? "Logging in..." 
+                    : "Log in"
+                  }
+                </button>
+              </div>
+            </Form>
     </div>
+  </div>
+</div>
   )
 }
