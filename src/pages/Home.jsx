83d2ad0f982fbda878d77618f9a912cd9bbdfc17
Iryna Howarth <irynahowarth@gmail.com>
2024-10-04 11:56:07 +0100

    [fix]: mistake in styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 lg:pt-32 text-center'>
-      <h2 className="max-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+      <h2 className="mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
         Authentification made simple for everyone
       </h2>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
